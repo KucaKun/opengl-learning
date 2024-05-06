d18d58cf6f012c8e46f9c33d2217d73fc4c5dbaa
Adrian Kuca <adriankuca2@gmail.com>
2024-05-06 17:42:21 +0200

    Smaller buffer size for a million vertexes

--- src/include/global_data.hpp
+++ src/include/global_data.hpp
@@ -3,7 +3,7 @@ namespace kckn {
     struct GlobalData {
         int window_width                    = 1920;
         int window_height                   = 1080;
-        unsigned int max_vertex_buffer_size = 0xfffffff;
+        unsigned int max_vertex_buffer_size = 0xffffff;
         unsigned int max_index_buffer_size  = 0xfffff;
     };
     static GlobalData global_data;
