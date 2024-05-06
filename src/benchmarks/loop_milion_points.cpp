496982bef5880b86bc5562aa26c3ccb60f8df19f
Adrian Kuca <adriankuca2@gmail.com>
2024-05-06 17:42:52 +0200

    Benchmark buffer upload

--- src/benchmarks/loop_milion_points.cpp
+++ src/benchmarks/loop_milion_points.cpp
@@ -27,24 +27,40 @@ BENCHMARK_DEFINE_F(RendererFixture, BM_MoveRandomly)(benchmark::State& state) {
     for (auto _ : state) {
         for (auto& point : points) {
             point->move_randomly();
+            benchmark::ClobberMemory();
         }
     }
 }
 
-BENCHMARK_DEFINE_F(RendererFixture, BM_UpdateBatchBuffers)(benchmark::State& state) {
-
+BENCHMARK_DEFINE_F(RendererFixture, BM_UploadWholeVertexBuffer)(benchmark::State& state) {
     auto points = object_manager->create_many<RandomParticle>(1000, 520, 520);
+    for (auto& point : points) {
+        point->move_randomly();
+    }
+    object_manager->update_batch_buffers();
+
     for (auto _ : state) {
-        for (auto& point : points) {
-            point->move_randomly();
-        }
-        object_manager->update_batch_buffers();
+        object_manager->get_vertex_buffer()->upload_whole_batch_buffer();
     }
 }
+
+BENCHMARK_DEFINE_F(RendererFixture, BM_UploadWholeIndexBuffer)(benchmark::State& state) {
+    auto points = object_manager->create_many<RandomParticle>(1000, 520, 520);
+    for (auto& point : points) {
+        point->move_randomly();
+    }
+    object_manager->update_batch_buffers();
+
+    for (auto _ : state) {
+        object_manager->get_index_buffer()->upload_whole_batch_buffer();
+    }
+}
+
 
 BENCHMARK_REGISTER_F(RendererFixture, BM_CreateParticlesSingle)->Unit(benchmark::kMillisecond);
 BENCHMARK_REGISTER_F(RendererFixture, BM_CreateParticlesBatch)->Unit(benchmark::kMillisecond);
 BENCHMARK_REGISTER_F(RendererFixture, BM_MoveRandomly)->Unit(benchmark::kMillisecond);
-BENCHMARK_REGISTER_F(RendererFixture, BM_UpdateBatchBuffers)->Unit(benchmark::kMillisecond);
+BENCHMARK_REGISTER_F(RendererFixture, BM_UploadWholeVertexBuffer)->Unit(benchmark::kMillisecond);
+BENCHMARK_REGISTER_F(RendererFixture, BM_UploadWholeIndexBuffer)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
